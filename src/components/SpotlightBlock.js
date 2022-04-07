cb71d21ca6b4cc64f08da1f53a86a550946f87c7
HongSenDu <hd2455@columbia.edu>
2022-04-06 20:57:59 -0400

    fixed spotlgith stylign

--- src/components/SpotlightBlock.js
+++ src/components/SpotlightBlock.js
@@ -1,64 +1,60 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const ArticleBoxWrapper = styled.div`
-    margin-left: 50px;
-    width: 100%;
-    display: flex;
+  margin-left: 50px;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  display: flex;
+  align-items: center;
+height:50vh;
+  padding-bottom: 50px;
+  color: ${(props) => props.theme.darkgraygit};
+  @media only screen and (min-width: 992px) {
     flex-direction: row;
-    flex-wrap: wrap;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    padding-bottom: 50px;
-    color: ${props => props.theme.darkgraygit};
-    @media only screen and (min-width: 992px){
-        flex-direction: row;
-        text-align: left;
-        color: ${props => props.theme.darkgray};
-    }
-    & div:first-of-type{
-        position: relative;
-        left: 0;
-        transition: left 0.3s;
-    }
-    & div:last-of-type{
-        position: relative;
-        right: 0;
-        transition: right 0.3s;
-    }
-    &:hover div:first-of-type{
-        left: 3px;
-    }
-    &:hover div:last-of-type{
-        right: 7px;
-    }
-`
+    text-align: left;
+    color: ${(props) => props.theme.darkgray};
+  }
+  & div:first-of-type {
+    position: relative;
+    left: 0;
+    transition: left 0.3s;
+  }
+  & div:last-of-type {
+    position: relative;
+    right: 0;
+    transition: right 0.3s;
+  }
+  &:hover div:first-of-type {
+    left: 3px;
+  }
+  &:hover div:last-of-type {
+    right: 7px;
+  }
+`;
 const YellowBox = styled.div`
-    background-color: #FFED37;
-    border-radius: 2vw;
-    width: 40vh;
-`
-
-const FlexChild = styled.div`
-    margin: 1%;
-`
+  background-color: #ffed37;
+  border-radius: 2vw;
+  width: 40vh;
+`;
 
 const ArticleImage = styled.div`
-    & a {
-        width: 0; //hide this: we only want the img to be clickable
-    }
-    & img {
-        overflow: hidden;
-        border-radius: 50%;
-        display: block;
-        width: 100%;
-        max-width: 50vh; /*actual image width*/
-        height: auto; /* maintain aspect ratio*/
-        margin: auto; /*optional centering of image*/
-    }
-`
+  & a {
+    width: 0; //hide this: we only want the img to be clickable
+  }
+  width: 20vw;
+  height: 20vw;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  background-image: url(${(props) => props.img});
+  background-size: cover;
+  background-position: center;
+  border-radius: 50%;
+`;
 
 const ArticleInfo = styled.div`
     width: 100%;
@@ -71,35 +67,50 @@ const ArticleInfo = styled.div`
         text-align: center;
         transform: rotate(-4.08deg);
     }
-`
+`;
 const Author = styled.div`
-    text-align: center;
-    margin-top: 1vw;
-    font-family: Bangers;
-    color: white;
-    opacity: 0.6;
-`
+  text-align: center;
+  margin-top: 1vw;
+  font-family: Bangers;
+  color: white;
+  opacity: 0.6;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 1;
+`;
+
+const T = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const SpotlightBlock = ({ id, data }) => {
-    return (
-        <ArticleBoxWrapper id={id}>
-            <FlexChild>
-                <YellowBox>
-                    <ArticleInfo>
-                        <a href={data.link}>
-                            <h2>{data.title}</h2>
-                        </a>
-                    </ArticleInfo>
-                    <ArticleImage>
-                        <a href={data.link}>
-                            <img src={data.img} alt={data.title} />
-                        </a>
-                    </ArticleImage>
-                </YellowBox>
-                <Author>{data.author}</Author>
-            </FlexChild>
-        </ArticleBoxWrapper>
-    )
-}
+  return (
+    <ArticleBoxWrapper id={id}>
+      <Column>
+        <YellowBox>
+          <ArticleInfo>
+            <a href={data.link}>
+              <h2>{data.title}</h2>
+            </a>
+          </ArticleInfo>
+          <T>
+            <a href={data.link}>
+              <ArticleImage img={data.img}>
+                {/* <img src={data.img} alt={data.title} /> */}
+              </ArticleImage>
+            </a>
+          </T>
+        </YellowBox>
+        <Author>{data.author}</Author>
+      </Column>
+    </ArticleBoxWrapper>
+  );
+};
 
 export default SpotlightBlock;
--- src/containers/Section.js
+++ src/containers/Section.js
@@ -5,7 +5,6 @@ import { MobileAndTablet, Desktop } from "react-responsive-simple";
 import ArticleBox from "../components/ArticleBox.js";
 import SpotlightBlock from "../components/SpotlightBlock";
 
-
 const SectionWrapper = styled.div`
   justify-content: space-between;
   background: ${(props) => props.theme.blue};
@@ -26,13 +25,19 @@ const ArticlesWrapper = styled.div`
   width: 100vw;
   @media only screen and (min-width: 992px) {
     width: 65vw;
-    margin-left:30%;
+    margin-left: 30%;
     padding: 4rem 0;
-    margin-top:-15vw;
+    margin-top: -15vw;
   }
   padding: 0rem 0;
 `;
 
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+`;
 
 const Section = ({ name, _ref, ad, miniTitle, blurb, data }) => {
   var list = [];
@@ -44,12 +49,20 @@ const Section = ({ name, _ref, ad, miniTitle, blurb, data }) => {
       name: el.title,
       description: el.author,
     };
-     if (name != "Spotlights")
-       return <ArticleBox key={i} data={el} id={id} />;
-     else 
-       return <SpotlightBlock key={i} data={el} id={id} />;
+    if (name != "Spotlights") return <ArticleBox key={i} data={el} id={id} />;
+    else {
+      if (i % 2 != 1 && i != 5)
+      return (
+        <Row>
+          <SpotlightBlock key={i} data={el} id={id} />
+          <SpotlightBlock key={i + 1} data={data[i + 1]} id={id} />
+        </Row>
+      );
+    }
+
     //return <ArticleBox key={i} data={el} id={id} />;
   });
+  console.log(Articles);
 
   const info = [
     {
@@ -65,13 +78,9 @@ const Section = ({ name, _ref, ad, miniTitle, blurb, data }) => {
   return (
     <SectionWrapper id={name} ref={_ref}>
       <HeaderWrapper>{TitleCards}</HeaderWrapper>
-      <ArticlesWrapper>
-        {Articles}
-      </ArticlesWrapper>
-
+      <ArticlesWrapper>{Articles}</ArticlesWrapper>
     </SectionWrapper>
   );
-
-}
+};
 
 export default Section;
